c6f998f7a97a75e178379958ac88facc68d55738
LamberJacks <ivzet009@gmail.com>
2026-02-08 16:37:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,109 @@
+(function(){
+  // плейсхолред на телефон
+  const telInputs = document.querySelectorAll("input[type='tel']")
+  const im = new Inputmask('+7 (999) 999-99-99')
+  im.mask(telInputs)
+  
+
+  // бургер меню
+
+    document.addEventListener("click", burgerInit)
+    function burgerInit(e) {
+      const body = document.body
+      const target = e.target
+      const navMobile = document.querySelector(".nav--mobile")
+      const burgerIcon = target.closest(".burger__menu")
+      const navLink = target.closest(".nav__item--mobile")
+      const navButton = target.closest(".header__button--mobile")
+      const width = document.documentElement.clientWidth
+      if (width <= 1200 && (burgerIcon || navLink || navButton)) {
+        body.classList.toggle("body--opened-menu")
+        navMobile.classList.toggle("nav--mobile--active")
+        burgerIcon.classList.toggle("burger__menu--active")
+      }
+    }
+
+        // ---------------------team_swiper-----------------------------
+
+// Левый слайдер (миниатюры)
+const thumbsSwiper = new Swiper('.team__swiper-left', {
+  spaceBetween: -10,
+  slidesPerView: 'auto',
+  freeMode: true,
+  watchSlidesProgress: true,
+  slideToClickedSlide: true,
+  
+  scrollbar: {
+    el: '.team__scrollbar',
+    draggable: true,
+  },
+  breakpoints: {
+  800: {
+    spaceBetween: 32
+  }
+}
+
+});
+
+// Функция показа члена команды
+function showTeamMember(index) {
+  // Все правые слайды
+  const rightSlides = document.querySelectorAll('.team__slide-right');
+  const leftSlides = document.querySelectorAll('.team__slide-left');
+  
+  // Прячем все
+  rightSlides.forEach(slide => {
+    slide.classList.remove('active');
+    // slide.style.opacity = '0';
+    slide.style.visibility = 'hidden';
+    
+  });
+  leftSlides.forEach(slide => {
+    slide.classList.remove('active-left');
+  });
+  
+  // Показываем нужный
+  if (rightSlides[index] && leftSlides[index]) {
+    rightSlides[index].classList.add('active');
+    leftSlides[index].classList.add('active-left');
+
+    // rightSlides[index].style.opacity = '1';
+    rightSlides[index].style.visibility = 'visible';
+  }
+}
+
+// Клик по миниатюре
+document.querySelectorAll('.team__slide-left').forEach((slide, index) => {
+  slide.addEventListener('click', () => {
+    thumbsSwiper.slideTo(index);
+    showTeamMember(index);
+  });
+});
+
+
+showTeamMember(0);
+
+
+ //  Акардион 
+
+  const accordionLists = document.querySelectorAll(".accordion-list")
+  accordionLists.forEach(el => {
+    el.addEventListener("click", e => {
+      const accordionList = e.currentTarget
+      const accordionOpen = accordionList.querySelector(".accordion-list__item--opened")
+      const accordionControl = e.target.closest(".accordion-list__control")
+
+      if (!accordionControl) return
+      e.preventDefault()
+      const accordionItem = accordionControl.parentElement;
+      if (accordionOpen && accordionItem != accordionOpen) {
+        accordionOpen.classList.remove("accordion-list__item--opened")
+      }
+
+      accordionItem.classList.toggle("accordion-list__item--opened")
+
+
+    })
+  })
+    
+})()
